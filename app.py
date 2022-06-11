7f576406a5c889fdfcb161bc06d261c5e8acd57f
Saketh Sundar <57919862+sakethsundar@users.noreply.github.com>
2022-06-11 09:32:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import pandas as pd
 import time
 with st.form(key='my_form'):
 	st.file_uploader(label="Upload a file", type=['png','jpg'])
-	lat = float(st.text_input('Enter the longitude coordinate of data collection'))
-	long = float(st.text_input('Enter the latitude coordinate of data collection'))
+	lat = st.text_input('Enter the longitude coordinate of data collection')
+	long = st.text_input('Enter the latitude coordinate of data collection')
 	submit_button = st.form_submit_button('Submit')
 if submit_button:
 	with st.spinner('Wait for it, this may take up to 30 seconds'):
@@ -21,8 +21,8 @@ if submit_button:
 	st.header('Quantification')
 	st.subheader('Five microplastics detected')
 	df = pd.read_csv('latlon.csv')
-	df2 = pd.DataFrame({"lat":[lat],
-                    "lon":[long]})
+	df2 = pd.DataFrame({"lat":[float(lat)],
+                    "lon":[float(long)]})
 	df = df.append(df2)
 	df = df.astype(float)
 	st.header('Map of Microplastic Data Collection Sites')
